c317b990bbc93d73d74fb243eb9b19c85301acee
shukill <shubhamjha.202@gmail.com>
2022-09-22 14:30:26 +0530

    project-setup

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import platform_device_id_macos
+import platform_device_id_v3
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
+  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
+}
